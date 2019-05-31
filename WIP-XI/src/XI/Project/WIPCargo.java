7daa04aaf047aded448be189e59155144a750a23
FameZXT <fam25452545@gmail.com>
2019-05-31 11:17:09 +0700

    Item

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -1,30 +1,63 @@
 /*
  * WIP XI Computer Programing 
  */
-
 package XI.Project;
+
+import java.util.Scanner;
 
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
+
     public static void main(String[] args) {
-        int age = 18;
-        String name = "Wippo";
-        double money = 1100.11;
-        
-        boolean online = true;
-        String status;
-        
-        System.out.println("Hi! my name is " + name + " i'm " + age + " Y/O ");
-        
-        if(online == true){
-            status = "Online";
-        } else {
-            status = "Offline";
+
+        int item = 5;
+        int limit = 10;
+        System.out.println("item" + item + ",limit" + limit);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1.Add");
+        System.out.println("2.Remove");
+        System.out.println("3.Check");
+        System.out.println("Input your menu");
+
+        int menu = input.nextInt();
+
+        if (menu == 1) {
+            if (item < limit) {
+                System.out.println("amout to add");
+                int amount = input.nextInt();
+                if (item + amount > limit) {
+                    System.out.println("ไอเท็มเกินลิมิต");
+                } else {
+                    item = item + amount;
+                    System.out.println("item" + item + ",limit" + limit);
+                }
+            } else {
+
+                System.out.println("ไอเท็มเต็มแล้ว");
+            }
         }
-        
-        System.out.println("I'm currently " + status + " and have " + money + "THB. left.");
+        if (menu == 2) {
+            System.out.println("Amount to Remove");
+            if (0 <= item && item <= limit) {
+                int amount = input.nextInt();
+                if (item - amount < 0) {
+                    System.out.println("ไอเท็มเไม่เพียงพอ");
+                } else {
+                    item = item - amount;
+                    System.out.println("item" + item + ",limit" + limit);
+                }
+            } else {
+
+                System.out.println("ไอเท็มไม่เพียงพอ");
+            }
+        }
+        if (menu == 3) {
+            System.out.println("item" + item + ",limit" + limit);
+        }
     }
 }
