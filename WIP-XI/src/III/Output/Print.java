32d9def302a7585ddfa3b9822b72ca38f008047f
FameZXT <fam25452545@gmail.com>
2019-05-31 08:44:18 +0700

    Add project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("OHH");
+        
+        System.out.print("My");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("GOD \n");
+        
+        System.out.print("gg");
+        
+    }
+}
---
+++ WIP-XI/src/IV/Operators/_1_Arithmetic.java
@@ -0,0 +1,18 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Arithmetic {
+    public static void main(String[] args) {
+        
+        System.out.println("หลิม ยูนิเวอส : " + (5*8/10));
+        
+        System.out.println("7+9-5*3 : " + (5+9*6/4-6));
+    }
+}
